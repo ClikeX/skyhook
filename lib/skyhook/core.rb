c7b11194b3c1eb203263686d7078d090c166de53
ClikeX <w.s.van.der.meulen@gmail.com>
2014-11-26 13:27:42 +0100

    refactored core and added base requests

--- lib/skyhook/core.rb
+++ lib/skyhook/core.rb
@@ -1,23 +1,49 @@
 module Skyhook
   class Core < Skyhook::Configuration
 
+    # These are default urls to use with #Service_request
     RESTAPI = {
       owned_games: "",
       shared_games: ""
     }.freeze
 
-    # TODO Recreate request methods
-    # TODO Put more code in the REST API
+    def api_reference
+      url = "ISteamWebAPIUtil/GetSupportedAPIList/v0001/"
+
+      unless @api_key.blank?
+        url << "?key=#{ self.api_key }"
+      end
+
+      request url
+    end
+
+    def resolve_vanity( vanityurl )
+      return vanityurl if vanityurl.is_a? Integer
+      request "ISteamUser/ResolveVanityURL/v0001/?key=#{ self.api_key }&vanityurl=#{ vanityurl }"
+    end
+
+    def user_summaries( steamids = [] )
+      steamids = [ steamids ] unless steamids.is_a? Array
+
+      raise( ArgumentError,  'No steamids were defined' ) if steamids.empty?
+
+      steamids.map do |steamid|
+        steamid = resolve_vanity steamid
+      end
+
+      request "ISteamUser/GetPlayerSummaries/v0002/?key=#{ self.api_key }&steamids=#{ steamids }"
+    end
 
     protected
-    def request(uri)
+
+    def request( uri )
       response = Net::HTTP.get_response BASE, uri
       JSON.parse response.body
     end
 
-    def service_request(uri, options = {})
-      raise(ArgumentError,  'No JSON options were supplied') if options.empty?
-      request("#{uri}?key=#{self.key}}&format=#{self.format}&input_json=#{options[:json]}")
+    def service_request( uri, options = {} )
+      raise( ArgumentError,  'No JSON options were supplied' ) if options.empty?
+      request "#{ uri }?key=#{ self.api_key }}&format=#{ self.format }&input_json=#{ options[:json]} "
     end
   end
 end
