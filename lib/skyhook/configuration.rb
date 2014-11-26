55f7f577145eca8a0c44bee1cf5a028352dda4f5
ClikeX <w.s.van.der.meulen@gmail.com>
2014-11-26 23:38:53 +0100

    updated configuration

--- lib/skyhook/configuration.rb
+++ lib/skyhook/configuration.rb
@@ -4,11 +4,11 @@ module Skyhook
     cattr_accessor :api_key
 
     FORMATS = %W(json xml vdf).map { |x| x.to_sym.freeze }
-    BASE = 'api.steampowered.com/'.freeze
+    BASE = 'api.steampowered.com'
 
-    def self.configure(options = {})
-      if options[:format]
-        FORMATS.include?(options[:format]) ? self.format = options[:format] : raise(ArgumentError,  "#{options[:format]} is not a valid format")
+    def self.configure( options = {} )
+      if options[ :format ]
+        FORMATS.include?( options[ :format ] ) ? self.format = options[ :format ] : raise(ArgumentError,  "#{ options[ :format ] } is not a valid format" )
       else
         @@format = :json
       end
