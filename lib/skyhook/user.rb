7582c248d7016084af7b954b30a11f85c609776c
ClikeX <w.s.van.der.meulen@gmail.com>
2014-11-26 23:59:00 +0100

    completed base for user class

--- lib/skyhook/user.rb
+++ lib/skyhook/user.rb
@@ -1,13 +1,39 @@
 module Skyhook
   class User < Skyhook::Core
 
-    attr_reader :steamid, :game, :response
+    attr_reader :steamid, :personaname, :realname, :profileurl
+    attr_reader :avatar, :avatarmedium, :avatarfull
+    attr_reader :communityvisibilitystate, :profilestate, :lastlogoff, :commentpermission
+    attr_reader :primaryclanid, :timecreated, :personastateflags, :personastate
+    attr_reader :loccityid, :locstatecode, :loccountrycode
+    attr_reader :response
 
     def initialize( steamid )
-      @response = user_summaries steamid
+      set_attributes user_summaries( steamid )["response"]["players"][0]
     end
 
-    # TODO Functionality
+    private
+
+    def set_attributes( response )
+      @steamid = response["steamid"]
+      @personaname = response["personaname"]
+      @communityvisibilitystate = response["communityvisibilitystate"]
+      @profilestate = response["profilestate"]
+      @lastlogoff = response["lastlogoff"]
+      @commentpermission = response["commentpermission"]
+      @profileurl = response["profileurl"]
+      @avatar = response["avatar"]
+      @avatarmedium = response["avatarmedium"]
+      @avatarfull = response["avatarfull"]
+      @personastate = response["personastate"]
+      @realname = response["realname"]
+      @primaryclanid = response["primaryclanid"]
+      @timecreated = response["timecreated"]
+      @personastateflags = response["personastateflags"]
+      @loccountrycode = response["loccountrycode"]
+      @locstatecode = response["locstatecode"]
+      @loccityid = response["loccityid"]
+    end
 
   end
 end
