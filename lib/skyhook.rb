4e91f88dfe236da3c6b8cd82c08bcb0ced3898b9
ClikeX <w.s.van.der.meulen@gmail.com>
2015-07-10 21:19:45 +0200

    added new files to skyhook.rb, added config for Faraday

--- lib/skyhook.rb
+++ lib/skyhook.rb
@@ -5,8 +5,19 @@ require 'skyhook/configuration'
 require 'skyhook/core'
 require 'skyhook/game'
 require 'skyhook/user'
+require 'skyhook/i_player_service'
+require 'skyhook/i_steam_apps'
+require 'skyhook/i_steam_economy'
+require 'skyhook/i_steam_news'
+require 'skyhook/i_steam_user'
+require 'skyhook/i_steam_user_stats'
+
+
 
 module Skyhook
+
+  Faraday::Utils.default_params_encoder = Faraday::FlatParamsEncoder
+
   def self.configure( options = {} )
     self::Configuration.configure options
   end
