1b9fc9a9f2bb434ca022e61187c052c0b7726577
ClikeX <w.s.van.der.meulen@gmail.com>
2015-07-10 23:20:49 +0200

    added remaining IPlayerService methods

--- lib/skyhook/i_player_service.rb
+++ lib/skyhook/i_player_service.rb
@@ -4,18 +4,43 @@ module Skyhook
     # IPlayerService
     # Methods relating to a Steam user's games.
 
-    def owned_games( steamid, options = {} )
-      steamid = resolve_vanity steamid
-      appinfo = options[:appinfo] == nil ? false : options[:appinfo]
-      free_games = options[:free_games] == nil ? false : options[:free_games]
+    def self.owned_games( steamid, options = {} )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+      appinfo = options[:appinfo].nil? ? false : options[:appinfo]
+      free_games = options[:free_games].nil? ? false : options[:free_games]
+      appids_filter = options[:appids_filter].nil? ? [] : options[:appids_filter]
 
-      parent.request "/IPlayerService/GetOwnedGames/v1/?steamid=#{ steamid }&include_appinfo='#{ appinfo }'&include_played_free_games='#{ free_games }'"
+      response = parent.request '/IPlayerService/GetOwnedGames/v1', steamid: steamid, include_appinfo: appinfo, include_played_free_games: free_games, appids_filter: appids_filter
+      response['response']
     end
 
-    def self.recently_played_games;end
-    def self.steam_level;end
-    def self.badges;end
-    def self.community_badge_progress;end
+    def self.recently_played_games( steamid, count = 10 )
+        steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+
+        response = parent.request '/IPlayerService/GetRecentlyPlayedGames/v1', steamid: steamid, count: count
+        response['response']
+    end
+
+    def self.steam_level( steamid )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+
+      response = parent.request '/IPlayerService/GetSteamLevel/v1', steamid: steamid
+      response['response']['player_level']
+    end
+
+    def self.badges( steamid )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+
+      response = parent.request '/IPlayerService/GetBadges/v1', steamid: steamid
+      response['response']
+    end
+
+    def self.community_badge_progress( steamid, badgeid = nil )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+
+      response = parent.request '/IPlayerService/GetCommunityBadgeProgress/v1', steamid: steamid, badgeid: badgeid
+      response['response']['quests']
+    end
 
   end
 end
