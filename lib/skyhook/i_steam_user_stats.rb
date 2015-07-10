93b35324596c3e218103e33c368612548e9f1645
ClikeX <w.s.van.der.meulen@gmail.com>
2015-07-11 00:17:32 +0200

    completed methods for ISteamUserStats

--- lib/skyhook/i_steam_user_stats.rb
+++ lib/skyhook/i_steam_user_stats.rb
@@ -4,21 +4,45 @@ module Skyhook
     # ISteamUserStats
     # Methods relating to User stats.
 
-    # TODO (ClikeX) Test this with multiple names.
-    def global_stats( appid, count = 1, names = [], options = {} )
-      parent.request "/ISteamUserStats/GetGlobalStatsForGame/v0001/?appidid=#{ appid }&count=#{}&name[0]=global.map.emp_isle"
+    def self.player_stats( appid, steamid )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+
+      response = parent.request '/ISteamUserStats/GetUserStatsForGame/v2', steamid: steamid, appid: appid
+      response['playerstats']
     end
 
-    def player_stats( appid, steamid )
-      steamid = resolve_vanity steamid
+    def self.player_achievements( appid, steamid, options = {} )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity steamid
+      l = options[:l] || options[:language]
 
-      parent.request "/ISteamUserStats/GetUserStatsForGame/v2/?steamid=#{ steamid }&appid=#{ appid }"
+      response = parent.request '/ISteamUserStats/GetPlayerAchievements/v1', appid: appid, steamid: steamid, l: l
+      response['playerstats']
     end
 
-    def global_achievenement_percentages;end
-    def number_of_current_players;end
-    def player_achievements;end
-    def schema_for_game;end
+    def self.number_of_current_players( appid )
+      response = parent.request '/ISteamUserStats/GetNumberOfCurrentPlayers/v1', appid: appid
+      response['response']['player_count']
+    end
 
+    # FIXME(ClikeX) this won't work without names. But names do use the [] url parameter encoding. This won't work with faraday
+    def self.global_stats( appid, count = 1, options = {} )
+      startdate = options[:startdate]
+      enddate = options[:enddate]
+
+      parent.request '/ISteamUserStats/GetGlobalStatsForGame/v0001', appidid: appid, count: count, startdate: startdate, enddate: enddate
+    end
+
+    def self.global_achievenement_percentages( appid )
+
+      response = parent.request '/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002', gameid: appid
+      response['achievementpercentages']['achievements']
+    end
+
+    def self.schema_for_game(appid, options = {})
+      l = options[:l] || options[:language]
+
+      result = parent.request '/ISteamUserStats/GetSchemaForGame/v2', appid: appid, l: l
+      result['game']
+    end
   end
 end
