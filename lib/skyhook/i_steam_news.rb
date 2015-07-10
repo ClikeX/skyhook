306c289720fe30ecf6c666f25c0548c6ad97caf2
ClikeX <w.s.van.der.meulen@gmail.com>
2015-07-10 23:40:51 +0200

    added ISteamNews methods

--- lib/skyhook/i_steam_news.rb
+++ lib/skyhook/i_steam_news.rb
@@ -4,7 +4,15 @@ module Skyhook
     # SteamNews
     # Methods relating to Steam News.
 
-    def news_for_app;end
+    def self.news_for_app( appid, options = {} )
+      maxlength = options[:maxlength]
+      enddate = options[:enddate]
+      count = options[:count]
+      feeds = options[:feeds]
+
+      response = parent.request '/ISteamNews/GetNewsForApp/v0002', appid: appid,  maxlength: maxlength, enddate: enddate, count: count, feeds: feeds
+      response['appnews']
+    end
 
   end
 end
