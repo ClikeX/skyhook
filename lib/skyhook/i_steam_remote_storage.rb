fda62d3dfa79f6a690aa2663e8e4a1d6d3c4b899
ClikeX <w.s.van.der.meulen@gmail.com>
2015-07-10 23:49:32 +0200

    added ugcf_file_details method to ISteamRemoteStorage

--- lib/skyhook/i_steam_remote_storage.rb
+++ lib/skyhook/i_steam_remote_storage.rb
@@ -4,9 +4,21 @@ module Skyhook
     # ISteamRemoteStorage
     # Methods relating to stored files.
 
+    # TODO finish implementing this
+
     def collection_details;end
     def published_file_details;end
-    def ugcf_file_details;end
+
+    def ugcf_file_details( ugcid, appid, options = {} )
+      steamid = Skyhook::Core::ISteamUser.resolve_vanity options[:steamid] unless options.empty?
+
+      response = parent.request 'ISteamRemoteStorage/GetUGCFileDetails/v1/', ugcid: ugcid, appid: appid, steamid: steamid
+      if response['status']['code'] == 9
+        raise StandardError
+      else
+        response['data']
+      end
+    end
 
   end
 end
