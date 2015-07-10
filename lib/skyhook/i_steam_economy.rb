710c56d9a4ba8112250062970dcd883caf0eaf5c
ClikeX <w.s.van.der.meulen@gmail.com>
2015-07-10 23:29:26 +0200

    added todo comment

--- lib/skyhook/i_steam_economy.rb
+++ lib/skyhook/i_steam_economy.rb
@@ -4,6 +4,8 @@ module Skyhook
     # ISteamEconomy
     # Methods relating to games' store's assets.
 
+    # TODO implement these methods
+
     def self.asset_class_info;end
     def self.asset_prices;end
 
